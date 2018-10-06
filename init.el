cd1c753a52e415e2df89aa0036fdc5d0e2d5b43e
iain <iain@soulflyer.co.uk>
2018-10-06 15:32:29 +0700

    added link to emacs goodies

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
   (let* ((config-dir "~/.emacs.d/config/"))
     (load-file (concat config-dir f-name))))
 
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/emacs-goodies-el/")
 (load-config-file "core.el")
 (load-config-file "packages.el")
 (load-config-file "bindings.el")
