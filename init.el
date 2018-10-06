eca868135a3012f8ad6d87c5a88f91f4b610593b
iain <iain@soulflyer.co.uk>
2018-10-06 11:24:59 +0700

    corrected path to config

--- init.el
+++ init.el
@@ -1,1 +1,6 @@
-(load-file "config/packages.el")
+(defun load-config-file (f-name)
+  "Load the config file with name"
+  (let* ((config-dir "~/emacs.d/config"))
+    (load-file (concat config-dir f-name))))
+
+(load-file "packages.el")
