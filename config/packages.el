bf9d06220391b6dc6b6a175c06d79b25ae10941e
iain <iain@soulflyer.co.uk>
2018-10-06 12:30:13 +0700

    add some packages

--- config/packages.el
+++ config/packages.el
@@ -3,6 +3,8 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
+(package-initialize)
+
 (package-install 'clojure-mode)
 (package-install 'cider)
 (package-install 'spinner)
