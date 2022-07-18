a21bfb10d81d8e5109691ebd5324e2a7f899d65b
Iain Wood <code@soulflyer.co.uk>
2022-07-18 12:51:18 +0700

    test

--- custom.el
+++ custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4c11dd74aeb1804f6c349699d5c4a86f840a93d36c04bd5ffb6b338e29d05a83" default)))
  '(package-selected-packages
    (quote
-    (spinner clojure-mode smex rainbow-delimiters paradox elisp-slime-nav counsel cider))))
+    (simpleclip spinner clojure-mode smex rainbow-delimiters paradox elisp-slime-nav counsel cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
