fd2045811d3fb7fde0f34bed4d5849be512a3be4
Iain <iain@soulflyer.co.uk>
2022-07-18 12:57:11 +0700

    test

--- custom.el
+++ custom.el
@@ -3,17 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4c11dd74aeb1804f6c349699d5c4a86f840a93d36c04bd5ffb6b338e29d05a83" default)))
  '(package-selected-packages
    (quote
-    (spinner clojure-mode smex rainbow-delimiters paradox elisp-slime-nav counsel cider))))
+    (simpleclip xclip delight swiper which-key cljr-ivy flx-ido use-package spinner clojure-mode tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode browse-kill-ring boxquote bm bar-cursor apache-mode smex rainbow-delimiters paradox elisp-slime-nav counsel cider)))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ediff-even-diff-A ((((class color) (background dark)) (:background "dark green"))))
- '(ediff-even-diff-B ((((class color) (background dark)) (:background "dark red"))))
- '(ediff-odd-diff-A ((((class color) (background dark)) (:background "dark green"))))
- '(ediff-odd-diff-B ((((class color) (background dark)) (:background "dark red"))))
- '(mumamo-background-chunk-major ((((class color) (background dark)) (:background "black"))))
- '(mumamo-background-chunk-submode1 ((((class color) (background dark)) (:background "black")))))
+ '(dired-perm-write ((t (:foreground "brightmagenta")))))
